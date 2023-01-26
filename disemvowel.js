6d24187638023294bf61614be4e344e464c7d68c
Darren Lin <92828291+MagicSalt@users.noreply.github.com>
2023-01-25 19:06:14 -0800

    Create disemvowel.js
    Solution to Disemvowel Trolls

---
+++ disemvowel.js
@@ -0,0 +1,13 @@
+function disemvowel(str) {
+  let string = str.replace(/a/g, '')
+                  .replace(/e/g, '')
+                  .replace(/i/g, '')
+                  .replace(/o/g, '')
+                  .replace(/u/g, '')
+                  .replace(/A/g, '')
+                  .replace(/E/g, '')
+                  .replace(/I/g, '')
+                  .replace(/O/g, '')
+                  .replace(/U/g, '');
+  return string;
+}
