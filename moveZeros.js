3f8b65134ebc713bf53d6983d860803c0417b111
Darren Lin <92828291+MagicSalt@users.noreply.github.com>
2023-01-31 21:41:16 -0800

    Update moveZeros.js
    Refactored solution

--- moveZeros.js
+++ moveZeros.js
@@ -1,17 +1,6 @@
-function moveZeros(arr) {
-  let result = [];
-  let count = 0;
-  
-  for (element of arr) {
-    if (element === 0) {
-      count++;
-    }
-    if (element !== 0) {
-      result.push(element);
-    }
-  }
-  for (let i = 0; i < count; i++) {
-    result.push(0);
-  }
-  return result;
+var moveZeros = function (arr) {
+  return [
+    ...(arr.filter(n => n !== 0)),
+    ...(arr.filter(n => n === 0))
+  ];
 }
