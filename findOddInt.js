49afaede6b6d4aec703989ec8949c0ff3bc1b7c4
MagicSalt <darrenlin815@hotmail.com>
2023-06-25 21:57:39 -0700

    Refactors solution to Find the odd int

--- findOddInt.js
+++ findOddInt.js
@@ -1,13 +1,1 @@
-function findOdd(A) {
-    let count = 0;
-    for (let i = 0; i < A.length; i++) {
-        for (let j = 0; j < A.length; j++) {
-            if (A[i] === A[j]) {
-                count++;
-            }
-        }
-        if (count % 2 !== 0) {
-            return A[i];
-        }
-    }
-}
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
