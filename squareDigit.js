23216dd757b3fbd35b62f2e02db5b52edfe5bd66
MagicSalt <darrenlin815@hotmail.com>
2023-06-27 17:32:03 -0700

    Refactors solution to Square Every Digit'

--- squareDigit.js
+++ squareDigit.js
@@ -1,8 +1,10 @@
 function squareDigits(num) {
-    let numStr = num.toString();
-    let result = '';
-    for (let i = 0; i < numStr.length; i++) {
-        result += Math.pow(parseInt(numStr[i]), 2);
-    }
-    return parseInt(result);
+    return Number(
+        ('' + num)
+            .split('')
+            .map(function (val) {
+                return val * val;
+            })
+            .join('')
+    );
 }
