79613c0a31485de5357b715dd311ebc38c1baf40
MagicSalt <darrenlin815@hotmail.com>
2023-07-07 23:16:32 -0700

    Refactors Who likes it? problem using switch and cases

--- likes.js
+++ likes.js
@@ -1,15 +1,19 @@
 function likes(names) {
-    let result = '';
-    if (names.length === 0) {
-        result = 'no one likes this';
-    } else {
-        result =
-            names.length === 1 ? `${names[0]} likes this` : `${names[0]} and ${names[1]} like this`;
-        if (names.length === 3) {
-            result = `${names[0]}, ${names[1]} and ${names[2]} like this`;
-        } else if (names.length > 3) {
-            result = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-        }
+    names = names || [];
+    switch (names.length) {
+        case 0:
+            return 'no one likes this';
+            break;
+        case 1:
+            return names[0] + ' likes this';
+            break;
+        case 2:
+            return names[0] + ' and ' + names[1] + ' like this';
+            break;
+        case 3:
+            return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+            break;
+        default:
+            return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
     }
-    return result;
 }
