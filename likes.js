3a50bcbdee1b28dd1172829117ab540e8221912e
MagicSalt <darrenlin815@hotmail.com>
2023-07-07 23:15:47 -0700

    Adds solution to Who likes it?

---
+++ likes.js
@@ -0,0 +1,15 @@
+function likes(names) {
+    let result = '';
+    if (names.length === 0) {
+        result = 'no one likes this';
+    } else {
+        result =
+            names.length === 1 ? `${names[0]} likes this` : `${names[0]} and ${names[1]} like this`;
+        if (names.length === 3) {
+            result = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        } else if (names.length > 3) {
+            result = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+        }
+    }
+    return result;
+}
